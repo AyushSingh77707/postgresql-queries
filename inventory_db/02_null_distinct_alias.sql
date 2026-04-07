eb4b913a3a513bc09a15ab748a39137babcdea35
AyushSingh77707 <ayushsingh77707@gmail.com>
2026-04-07 10:40:39 +0530

    Add Stage 2: null handling, distinct, aliases

---
+++ inventory_db/02_null_distinct_alias.sql
@@ -0,0 +1,20 @@
+-- Q1: Fetch distinct categories in product
+SELECT DISTINCT category FROM products;
+
+-- Q2: Fetch every product and their prices and keep alias of price as "Product Price" and name as "Product Name"
+SELECT name AS Product_Name,
+price AS Product_Price 
+FROM products;
+
+-- Q3: Fetch distinct roles exists in user table
+SELECT DISTINCT role FROM users;
+
+-- Q4: Fetch those products details whose description is null
+SELECT * from products
+WHERE description IS NULL;
+
+-- Q5: Show no description using COALESCE of those product whose description is empty
+SELECT name,
+COALESCE(description,'No description available')
+FROM products;
+ 
